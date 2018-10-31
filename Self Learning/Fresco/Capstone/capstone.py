30d969266efaefc1fec8c89f576ae6f19d2125ce
Baradh <pcbaradhwaj@gmail.com>
2018-10-31 09:00:30 +0530

    100 days ml day 0 - Preping up

--- Self Learning/Fresco/Capstone/capstone.py
+++ Self Learning/Fresco/Capstone/capstone.py
@@ -10,6 +10,16 @@ import csv
 import os 
 import string
 import re
+from nltk.classify.util import apply_features
+from nltk import NaiveBayesClassifier
+import pandas as pd
+import pickle
+import string
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk import word_tokenize, WordNetLemmatizer
+from sklearn.model_selection import train_test_split
 
 #Data Loading
 os.getcwd()
@@ -21,9 +31,16 @@ stop_words = set(stopwords.words('english'))
 morewords = ['Subject:']
 stop_words.update(morewords)
 
-new_words = []
-        
-def extract_tokens(text, target):
+from nltk.classify.util import apply_features,accuracy
+from nltk import NaiveBayesClassifier
+import pandas as pd
+import pickle
+
+
+class SpamClassifier:
+    uniqueWords = []
+    classifier = 0
+    def extract_tokens(self, text, target):
         """returns array of tuples where each tuple is defined by (tokenized_text, label)
          parameters:
                 text: array of texts
@@ -35,36 +52,33 @@ def extract_tokens(text, target):
         print(len(text))
         i=0
         stop_words = set(stopwords.words('english'))
+        morewords = ['Subject']
+        stop_words.update(morewords)
         for t in text:
-            for l in target:
+            for res in target:
                 tokens = nltk.word_tokenize(t) # tokenize the text'
-                tokens = list(filter(lambda x: x.lower() not in stop_words and len(x) >=3 and x.lower() != "subject", tokens)) # remove stop words
+                tokens = list(filter(lambda x: x.lower() not in stop_words and len(x) >=3, tokens))
                 #tokens = [w.lower() for w in tokens if len(w) >=3]
-                
                 tokens = [w for w in tokens if re.search('[a-zA-Z]', w)]
-                dataTuple = (tokens,l)     
+                dataTuple = (tokens,res)
             tokenarray.append(dataTuple)
             i=i+1
         return tokenarray
-tokenArray = extract_tokens(messages,[0,1])    
-    
-def get_features(corpus):
-        """ 
-        returns a Set of unique words in complete corpus. 
-        parameters:- corpus: tokenized corpus along with target labels
         
+    
+    def get_features(self, corpus):
+        """
+        returns a Set of unique words in complete corpus.
+        parameters:- corpus: tokenized corpus along with target labels
         Return Type is a set
         """
-        uniqueWords = []
         for tokens, labels in corpus:
             for item in tokens:
-                if item not in uniqueWords:
-                    uniqueWords.append(item)
-        return set(unique_word_list)
-wordList =  get_features(tokenArray)
-
-from nltk.tokenize import word_tokenize # or use some other tokenizer
-def extract_features(document):
+                if item not in SpamClassifier.uniqueWords:
+                    SpamClassifier.uniqueWords.append(item)
+        return SpamClassifier.uniqueWords
+        
+    def extract_features(self, document):
         """
         maps each input text into feature vector
         parameters:- document: string
@@ -72,28 +86,42 @@ def extract_features(document):
         Return type : A dictionary with keys being the train data set word features.
                       The values correspond to True or False
         """
-        
-        #all_words = set(word.lower() for passage in document for word in word_tokenize(passage[0]))
-        #dictWords = [({word: (word in word_tokenize(x[0])) for word in all_words}, x[1]) for x in document]
-                      
+        features = {}
         for word in document:
-            
-            
-        return dictWords     
-dictWords = extract_features(wordList)
+            if word in SpamClassifier.uniqueWords:
+                features[word] = True
+            else:
+                features[word] = False
+        return features
+
+
     def train(self, text, labels):
         """
         Returns trained model and set of unique words in training data
         """
-        
+        extractTokens = []
+        extractTokens = self.extract_tokens(text, labels)
+        result = self.get_features(extractTokens)
+        trainedDataSet = nltk.classify.apply_features(self.extract_features,extractTokens)
+        classifier = nltk.NaiveBayesClassifier.train(trainedDataSet)
+        return classifier, result
     
     def predict(self, text):
         """
-        Returns prediction labels of given input text. 
+        Returns prediction labels of given input text.
         Allowed Text can be a simple string i.e one input email, a list of emails, or a dictionary of emails identified by their labels.
         """
-        
-    
+        #accuracyPercent = nltk.classify.accuracy(self.classifier, text)*100
+        #predictedData = self.classifier.classify(text)
+        #return predictedData,accuracyPercent
+        testTokens = []
+        testTokens = self.extract_tokens(text, [0,1])
+        result = self.get_features(testTokens)
+        testDataSet = nltk.classify.apply_features(self.extract_features,testTokens)
+        accuracyPercent = nltk.classify.accuracy(self.classifier, testDataSet)*100
+        predictedData = self.classifier.classify(testDataSet)
+        return predictedData,accuracyPercent
+  
     
 if __name__ == '__main__':
     
@@ -106,35 +134,15 @@ if __name__ == '__main__':
                                                             stratify=data["spam"].values)
     classifier = SpamClassifier()
     classifier_model, model_word_features = classifier.train(train_X, train_Y)
+    predicted,accuracy = classifier.predict(test_X)
+    print(accuracy)
     model_name = 'spam_classifier_model.pk'
     model_word_features_name = 'spam_classifier_model_word_features.pk'
     with open(model_name, 'wb') as model_fp:
         pickle.dump(classifier_model, model_fp)
     with open(model_word_features_name, 'wb') as model_fp:
             pickle.dump(model_word_features, model_fp)
-
-            
-            
-
-
-    def extract_tokens(text, target):
-        """returns array of tuples where each tuple is defined by (tokenized_text, label)
-         parameters:
-                text: array of texts
-                target: array of target labels
-                
-        NOTE: consider only those words which have all alphabets and atleast 3 characters.
-        """
-        stop_words = set(stopwords.words('english'))
-        regex = re.compile('[' +re.escape(string.punctuation) + '0-9\\r\\t\\n]') #remove punctuation
-        text = regex.sub(' ', text)
-        tokens = nltk.word_tokenize(text) # tokenize the text
-        tokens = list(filter(lambda x: x.lower() not in stop_words, tokens)) # remove stop words
-        tokens = [w.lower() for w in tokens if len(w) >=3]
-        tokens = [w for w in tokens if re.search('[a-zA-Z]', w)]
-        return tokens
-extract_tokens(messages,[0,1])
-
+    
 tupArray = []
 for word in messages:
     for l in [0,1]:
